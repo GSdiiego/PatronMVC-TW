120b48a9726d8da833c0d7a70d87ab89b67f0fb6
GSdiiego <gsdiegohp@outlook.com>
2026-03-23 20:06:39 -0600

    Practica 1.1

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+//https: // protocolo
+//www.pagina.com // dominio
+//watch // layout 
+//? // envio de variables por metodo get 
+//v= // envio de variables por metodo get 
+//7LD9-wofjI // 
+//& // separador de varibles 
+
+$nombre = "Diego";
+$Game = "PUBG mobile";
+$estatura = "1.69";
+$edad = "23 años";
+$carrera = "Ingenieria en sistemas";
+$universidad = "UPEM";
+$hogar = "Av Santa Lucia";
+$municipio = "Tecamac";
+$pais = "Mexico";
+$nacionalidad = "Mexicano";
+
+
+echo"mi nombre es: ".$nombre;
+echo"\nMi videojuego favorito es:".$Game;
+echo"\nMi estatura es de:".$estatura;
+echo"\nMi edad:".$edad;
+echo"\nEstudio la carrera de:".$carrera;
+echo"\nEn la universidad:".$universidad;
+echo"\nDomicilio:".$hogar;
+echo"\nVivo en el municipio de:".$municipio;
+echo"\nPais de origen:".$pais;
+echo"\nMi nacionalidad:".$nacionalidad;
+?>
